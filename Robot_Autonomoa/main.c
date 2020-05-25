73d395aaf00eb935c8d8f849b7389a661b07e2d9
9ekaitz <ekaitz.casado@alumni.mondragon.edu>
2020-05-25 08:51:54 +0200

    matrizena erreflexibitatea konponduta

--- Robot_Autonomoa/main.c
+++ Robot_Autonomoa/main.c
@@ -16,17 +16,17 @@ void copyPath(int *destVertex, int *destLen, int *srcVertex, int srcLen);
 
 int main()
 {
-	int infinite = INT_MAX, start = 0, end = 4;
+	int infinite = INT_MAX, start = 7, end = 4;
 	PATH path;
 uint grafiko[KOP][KOP] = {
-		{infinite,10,3,8,infinite,infinite,infinite,infinite},		//A
-		{10,0,8,infinite,6,infinite,infinite,infinite},		//B
-		{3,8,0,4,infinite,9,infinite,infinite},				//C
-		{8,infinite,4,0,infinite,infinite,7,infinite},		//D
-		{infinite,6,infinite,infinite,0,3,infinite,11},		//E
-		{infinite,infinite,9,infinite,3,0,1,8},				//F
-		{infinite,infinite,infinite,7,infinite,1,0,5},		//G
-		{infinite,infinite,infinite,infinite,11,8,5,0}		//H
+		{infinite,10,3,8,infinite,infinite,infinite,infinite},			//A
+		{10,infinite,8,infinite,6,infinite,infinite,infinite},			//B
+		{3,8,infinite,4,infinite,9,infinite,infinite},					//C
+		{8,infinite,4,infinite,infinite,infinite,7,infinite},			//D
+		{infinite,6,infinite,infinite,infinite,3,infinite,11},			//E
+		{infinite,infinite,9,infinite,3,infinite,1,8},					//F
+		{infinite,infinite,infinite,7,infinite,1,infinite,5},			//G
+		{infinite,infinite,infinite,infinite,11,8,5,infinite}			//H
 };
 
 	path = dijkstra(grafiko, start, end);
