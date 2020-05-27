e18367d3375aa34aeedb94384ec7ef1117ac2f82
gorkalertxundi <i>
2020-05-27 12:52:01 +0200

    Scrolling v0.5

--- Robot_Autonomoa/main.c
+++ Robot_Autonomoa/main.c
@@ -1,137 +1,92 @@
 #include "SDL2/SDL.h"
 #include <stdio.h>
 #include <stdlib.h>
+#define SCREEN_W 1280
+#define SCREEN_H 720
+#define MAX_X_PHOTO 10210
+#define MAX_Y_PHOTO 4993
+#define BPP 50
 
-#define MAX_STR 128
-
-//Gorka
-
-typedef enum BOOLEANOA {FALSE, TRUE};
-
-typedef struct Nodoa{
-	char izena;
-	int bisitatua;
-	int amaitua;
-	int bide_kant;
-	char aurrekoa;
-	struct Ertza* gertuena;
-	struct Nodoa* ptrHurrengoa;
-}NODOA;
-
-typedef struct Ertza{
-	struct Nodoa* ertza;
-	int pisua;
-	struct Ertza* ptrHurrengoa;
-}ERTZA;
-
-typedef struct Zerrenda{
-	struct Nodoa* izena;
-	struct Zerrenda* ptrHurrengoa;
-}ZERRENDA;
-
-
+//Gorka Scrolling
 int main(int argc, char* argv[])
 {
-	NODOA* nodohasiera = NULL;
-	ZERRENDA* zerrendahasiera = NULL;
-	ZERRENDA* zerrendaamaiera = NULL;
+	SDL_Init(SDL_INIT_EVERYTHING);
 
-    return 0;
-}
-void ErpinaHasieratu(NODOA* berria)
-{
-	berria->ptrHurrengoa = NULL;
-	berria->gertuena = NULL;
-	berria->bisitatua = FALSE;
-	berria->amaitua = FALSE;
-	berria->bide_kant = -1;
-	berria->aurrekoa = 0;
-}
-void NodoanSartu(NODOA* nodohasiera)
-{
-	NODOA* aux;
-	NODOA* berria = (NODOA*)malloc(sizeof(NODOA));
-	char str[MAX_STR];
+	SDL_bool running = SDL_TRUE;
+    SDL_Window *window;                    // Declare a pointer
+    SDL_Surface* background;
+    SDL_Rect camera;
+    int x = 0, y = 0, speed = 5, FPS = 30;
+    camera.x = 0;
+    camera.y = 0;
+    camera.w = SCREEN_W;
+    camera.h = SCREEN_H;
+    Uint32 start;
 
-	printf("Sartu mapako puntuaren izena: ");
-	fgets(str, 128, stdin);
-	sscanf(str, "%c", &berria->izena);
+    background = SDL_LoadBMP("./media/background.bmp");
 
-	ErpinaHasieratu(&berria);
+    //screen = SDL_SetVideoMode(SCREEN_W, SCREEN_H, BPP, SDL_SWSURFACE); //SDL_SWSURFACE: Create the video surface in system memory
+    window = SDL_CreateWindow(
+    		"OHM CARS SYSTEM",                  			// Izenburua
+    		SDL_WINDOWPOS_CENTERED,           // Leihoa erdian jarri
+    		SDL_WINDOWPOS_CENTERED,           // Leihoa erdian jarri
+    		SCREEN_W,                // Sortzeko lehioaren zabalera
+    		SCREEN_H,                 // Sortzeko leihoaren altuera
+    		SDL_WINDOW_OPENGL                // Erabilitako teknologia grafikoa
+    	);
 
-	if (nodohasiera == NULL)
-	{
-		nodohasiera = berria;
-	}
-	else {
-		aux = berria;
-		while(aux->ptrHurrengoa != NULL)
-		{
-			aux = aux->ptrHurrengoa;
-		}
-		aux->ptrHurrengoa = berria;
-	}
-
-}
-
-void ErtzaSartu(NODOA* nodohasiera)
-{
-	char A, B, str[MAX_STR];
-	NODOA* aux = nodohasiera;
-	NODOA* aux2 = nodohasiera;
-	ERTZA* berria = (ERTZA*)malloc(sizeof(ERTZA));
-
-	berria->ptrHurrengoa = NULL;
-
-	if(nodohasiera == NULL)
-	{
-		printf("Error: Grafoa utsik dago!\n");
-	}
-
-	printf("Sartu ertzaren hasiera puntua: ");
-	fgets(str, MAX_STR, stdin);
-	sscnaf(str, "%c", &A);
-	printf("Sartu ertzaren amaiera puntua: ");
-	fgets(str, MAX_STR, stdin);
-	sscnaf(str, "%c", &B);
-
-	printf("Sartu ertzaren pisua: ");
-	fgets(str, MAX_STR, stdin);
-	sscanf(str, "%d", &berria->pisua);
-
-	//Konprobatu behar ea ondo dagoen
-	//AMAITZEKO
-}
-
-void Dijkstra(NODOA* nodohasiera)
-{
-	NODOA* aux = nodohasiera;
-	char A, B;
-
-	while (aux != NULL)
-	{
-		if(aux->izena == A)
-		{
-			aux->amaitua = TRUE;
-			aux->bide_kant = 0;
-			break;
-		}
-		aux = aux->ptrHurrengoa;
-	}
-	if (aux == NULL)
-	{
-		printf("Error: Erpina ez da aurkitu!\n");
-		return;
-	}
-	while (aux != NULL)
-	{
-		ERTZA* aux2 = aux->gertuena;
-		while (aux2 != NULL)
-		{
-			if((aux2->ertza->bide_kant == -1) || (aux->bide_kant + aux2->pisua < aux2->ertza->bide_kant) ) //Ertz hau ez bada horaindik bisitatu edo
+    while (running)
+    {
+    	SDL_Event event;
+    	while(SDL_PollEvent(&event))
+    	{
+    		switch(event.type)
 			{
-
+    		case SDL_QUIT:
+    			running = SDL_FALSE;
+    			break;
+    		case SDL_KEYUP:
+    			switch (event.key.keysym.sym)
+    			{
+    			case SDLK_RIGHT:
+    				if(MAX_X_PHOTO-SCREEN_W >= camera.x)
+    				{
+    					x += speed;
+    					camera.x += speed;
+    				}
+    				break;
+    			case SDLK_LEFT:
+    				if(camera.x >= 0)
+    				{
+    					x -= speed;
+    					camera.x -= speed;
+    				}
+    				break;
+    			case SDLK_UP:
+    				if(MAX_Y_PHOTO-SCREEN_H >= camera.y)
+    				{
+    				    y += speed;
+    				    camera.y += speed;
+    				}
+    				break;
+    			case SDLK_DOWN:
+    				if(camera.y >= 0)
+    				{
+    				   	y -= speed;
+    					camera.y -= speed;
+    				}
+    				break;
+    			}
 			}
-		}
-	}
+    	}
+    	SDL_BlitSurface(background, &camera, window, NULL);
+    	SDL_Flip(window);
+    	if(1000/FPS>SDL_GetTicks()-start);
+    	{
+    		SDL_Delay(1000/FPS-(SDL_GetTicks()-start));
+    	}
+    }
+
+    SDL_Quit();
+    return 0;
 }
